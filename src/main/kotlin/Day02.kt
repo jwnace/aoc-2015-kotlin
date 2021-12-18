32161822dadea59a6b366f15ccde73610fcde044
Joe Nace <jwnace@gmail.com>
2021-12-17 21:13:06 -0500

    solution for day 02

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -4,11 +4,39 @@ fun main() {
     val input = File("input/day02.txt").readLines()
 
     fun part1() {
-        println("Day 02, Part 1: ")
+        var total = 0
+
+        for (line in input) {
+            val values = line.split('x').map { it.toInt() }
+            val (l, w, h) = values
+
+            val a = l * w
+            val b = w * h
+            val c = h * l
+
+            val surfaceArea = 2*a + 2*b + 2*c
+            val smallestSide = minOf(a, b, c)
+
+            total += surfaceArea + smallestSide
+        }
+
+        println("Day 02, Part 1: $total")
     }
 
     fun part2() {
-        println("Day 02, Part 2: ")
+        var total = 0
+
+        for (line in input) {
+            val values = line.split('x').map { it.toInt() }.sorted()
+            val (a, b, c) = values
+
+            val perimeter = 2*a + 2*b
+            val bow = a*b*c
+
+            total += perimeter + bow
+        }
+
+        println("Day 02, Part 2: $total")
     }
 
     part1()
