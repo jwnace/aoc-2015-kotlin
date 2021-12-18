a82add0f52b93329bd092367a86c98ffce75ae58
Joe Nace <jwnace@gmail.com>
2021-12-17 21:08:04 -0500

    solution for day 01

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,14 +1,25 @@
 import java.io.File
 
 fun main() {
-    val input = File("input/day01.txt").readLines()
+    val input = File("input/day01.txt").readText()
 
     fun part1() {
-        println("Day 01, Part 1: ")
+        val answer = input.count { it == '(' } - input.count { it == ')' }
+
+        println("Day 01, Part 1: $answer")
     }
 
     fun part2() {
-        println("Day 01, Part 2: ")
+        var floor = 0
+
+        for (i in input.indices) {
+            floor += if (input[i] == '(') 1 else -1
+
+            if (floor == -1) {
+                println("Day 01, Part 2: ${i + 1}")
+                return
+            }
+        }
     }
 
     part1()
