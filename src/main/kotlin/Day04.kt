cfc5ca9b228b3a5dd86a079200bd4baa0486d678
Joe Nace <jwnace@gmail.com>
2021-12-17 22:12:38 -0500

    solution for day 04

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,16 +1,34 @@
 import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
 
 fun main() {
-    val input = File("input/day04.txt").readLines()
+    val input = File("input/day04.txt").readText()
+
+    fun solve(prefix: String): Int {
+        for (i in 0..Int.MAX_VALUE) {
+            val answer = (input + i).md5()
+
+            if (answer.startsWith(prefix)) {
+                return i
+            }
+        }
+
+        return 0
+    }
 
     fun part1() {
-        println("Day 04, Part 1: ")
+        val answer = solve("00000")
+        println("Day 04, Part 1: $answer")
     }
 
     fun part2() {
-        println("Day 04, Part 2: ")
+        val answer = solve("000000")
+        println("Day 04, Part 1: $answer")
     }
 
     part1()
     part2()
 }
+
+fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16).padStart(32, '0')
