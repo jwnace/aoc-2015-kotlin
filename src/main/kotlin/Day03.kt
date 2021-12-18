b955bc28cb451f7bc3e6ccea0a316b0b8f1570ff
Joe Nace <jwnace@gmail.com>
2021-12-17 21:35:03 -0500

    solution for day 03

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,14 +1,64 @@
 import java.io.File
 
 fun main() {
-    val input = File("input/day03.txt").readLines()
+    val input = File("input/day03.txt").readText()
 
     fun part1() {
-        println("Day 03, Part 1: ")
+        val houses = mutableListOf(0 to 0)
+
+        var x = 0
+        var y = 0
+
+        for (i in input.indices) {
+            when (input[i]) {
+                '^' -> y++
+                'v' -> y--
+                '<' -> x--
+                '>' -> x++
+            }
+
+            houses.add(x to y)
+        }
+
+        val answer = houses.groupBy { it }.count()
+
+        println("Day 03, Part 1: $answer")
     }
 
     fun part2() {
-        println("Day 03, Part 2: ")
+        var santaX = 0
+        var santaY = 0
+
+        var robotX = 0
+        var robotY = 0
+
+        val houses = mutableListOf(0 to 0, 0 to 0)
+
+        for (i in 0 until input.length step 2) {
+            when (input[i]) {
+                '^' -> santaY++
+                'v' -> santaY--
+                '<' -> santaX--
+                '>' -> santaX++
+            }
+
+            houses.add(santaX to santaY)
+        }
+
+        for (i in 1 until input.length step 2) {
+            when (input[i]) {
+                '^' -> robotY++
+                'v' -> robotY--
+                '<' -> robotX--
+                '>' -> robotX++
+            }
+
+            houses.add(robotX to robotY)
+        }
+
+        val answer = houses.groupBy { it }.count()
+
+        println("Day 03, Part 2: $answer")
     }
 
     part1()
